aa8c8f2f4c1df5f989c0ef308717f529015cb5c1
Justin Carruthers <jtcruthers@gmail.com>
2016-06-28 23:50:57 -0400

    Fully written, untested

--- test.py
+++ test.py
@@ -12,8 +12,34 @@ access_token_secret = 'cW0FDtFwG4b8IJgCBhRA0M8lHl80X3tBcu98co4xhFr4N'
 auth = tweepy.OAuthHandler(consumer_key, consumer_secret)
 auth.set_access_token(access_token, access_token_secret)
 
-#Get the api object and timestamp
+#Get the api object and set name 
 api = tweepy.API(auth)
-timeline = api.user_timeline("HillaryClinton")
+handle = "CillaryHancer"
 
+#Read id from file and timeline
+lastID = int(readID())
+tweetID = getTweetID(api, handle)
 
+if lastID < tweetID:
+    respond(api, tweetID)
+    writeID(tweetID)
+    
+def readID():
+    file = open('lastid', 'r')
+    str = file.read()
+    file.close()
+    
+    return str
+
+def writeID(newID):
+    file = open('lastid', 'w')
+    file.write(newID)
+    file.close
+    
+def respond(api, tweetID):
+    #Read random fact
+    api.update_status("@CillaryHancer Bad " +  str(tweetID), in_reply_to_status_id = tweetID)
+
+def getTweetID(api, handle):
+    timeline = api.user_timeline(handle, count = 1)
+    return timeline[0].id
